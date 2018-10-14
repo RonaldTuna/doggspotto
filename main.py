bdec81a24b91fb11f4fa5b86c7b0bef07cc0a588
ddl004 <msonetwothree@gmail.com>
2018-10-13 23:31:27 -0700

    update main.py
    lots of changes

--- main.py
+++ main.py
@@ -13,23 +13,51 @@
 # limitations under the License.
 
 # [START gae_python37_app]
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
+from clarifai.rest import ClarifaiApp, Concept
+from clarifai.rest import Image as ClImage
+
+app_c = ClarifaiApp(api_key='d4e760d1e96940fb9a371f79f3249bc5')
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
 
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
 
-@app.route('/')
+def get_label(image_url):
+	model = app_c.models.get('doggospotto')
+	#image = ClImage(url=image_url)
+	full_name = '/static/img/' + image_url
+	image = ClImage(file_obj=open(full_name, 'rb'))
+	response_data = model.predict([image])
+
+	print("You have uploaded a: ")
+	return response_data['outputs'][0]['data']['concepts'][0]['name']
+
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    """Return a friendly HTTP greeting."""
-    return render_template('index.html', image = "doggo_guide.png")
+		return render_template('index.html')
 
+@app.route('/uploaded', methods = ['GET', 'POST'])
+def uploaded():	
+	if request.method == 'POST' and 'photo' in request.files:
+		filename = photos.save(request.files['photo'])
+		print("waiting for clarifai")
+		output = get_label(filename)
+		# return filename
 
+		return render_template('photo_uploaded.html', img = filename, category = output)
+
+#{{ url_for('hello') }}
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+	# This is used when running locally only. When deploying to Google App
+	# Engine, a webserver process such as Gunicorn will serve the app. This
+	# can be configured by adding an `entrypoint` to app.yaml.
+	app.run(host='127.0.0.1', port=5000, debug=True)
 # [END gae_python37_app]
+
